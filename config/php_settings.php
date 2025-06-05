5d66666bf51a13dbd15ae927c0d21fb1742f256c
simoncode12 <123048201+simoncode12@users.noreply.github.com>
2025-06-05 15:36:31 +0700

    Update php_settings.php

--- config/php_settings.php
+++ config/php_settings.php
@@ -10,22 +10,16 @@ ini_set('post_max_size', '10M');
 ini_set('memory_limit', '256M');
 ini_set('max_execution_time', '30');
 
-// Error reporting based on environment
-if (DEBUG_MODE) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', 0);
-    ini_set('display_startup_errors', 0);
-    error_reporting(0);
-}
+// Default error reporting (will be overridden later if DEBUG_MODE is defined)
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 
 // Session configuration
 ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1);
 ini_set('session.use_strict_mode', 1);
-ini_set('session.gc_maxlifetime', SESSION_LIFETIME);
+ini_set('session.gc_maxlifetime', 7200); // Default 2 hours, will be overridden
 
 // File upload security
 ini_set('file_uploads', 1);
